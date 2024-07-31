d13720bef2ce858facfb0adb850b7c3e971eb3aa
Skyfall1998 <zhigaylovmaxim@gmail.com>
2024-07-31 18:35:02 +0700

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input('max')
+print(input())
+print('my_string'.upper())
+print('my_string'.lower())
+print('my_string'.replace(' ', '#'))
+print('my_string'.replace('my_string', 'm'))
+print('my_string'.replace('my_string', 'g'))
